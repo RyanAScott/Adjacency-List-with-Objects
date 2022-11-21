fb0a30fb0c141fbd08879d6334bdbc56c63b1b4c
RyanAScott <ryanandrewscott@gmail.com>
2022-11-21 01:02:41 -0500

    Saving..

--- Graph.java
+++ Graph.java
@@ -10,18 +10,35 @@ public class Graph{
     }// Graph Constructor
 
     public void addNode(Node node){
-        
+        LinkedList<Node> currentList = new LinkedList<>();
+        currentList.add(node);
+        alist.add(currentList);
     }// addNode
 
     public void addEdge(int src, int dst){
-
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).get(0);
+        currentList.add(dstNode);
     }// addEdge
 
-    public void checkEdge(int src, int dst){
+    public boolean checkEdge(int src, int dst){
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).get(0);
 
+        for(Node node : currentList) {
+            if(node == dstNode) {
+                return true;
+            }// if
+        }// for
+        return false;
     }// checkEdge
 
     public void print(){
-
+        for(LinkedList<Node> currentList : alist){
+            for(Node node : currentList){
+                System.out.print(node.data + " -> ");
+            }// for
+            System.out.println();
+        }// for
     }// print
 }// Graph Class
